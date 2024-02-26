00d0b2971ebb680cb75df1fc716f71641a300e20
broma0 <121064585+broma0@users.noreply.github.com>
2024-02-26 18:53:36 +0000

    initial commit

---
+++ make.lua
@@ -0,0 +1,28 @@
+local env = {
+
+  name = "santoku-matrix",
+  version = "0.0.1-1",
+  variable_prefix = "TK_MATRIX",
+  public = true,
+
+  dependencies = {
+    "lua == 5.1",
+    "santoku >= 0.0.196-1",
+  },
+
+  test = {
+    dependencies = {
+      "luacov == 0.15.0-1",
+    }
+  },
+
+}
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
+
+return {
+  type = "lib",
+  env = env,
+}
