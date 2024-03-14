20b927db5d350896b53122c5e1058e49ca5d3b69
Matthew Brooks <mb964033@gmail.com>
2024-03-14 20:02:18 +0000

    add create 1 by #t matrix from number table t

--- test/spec/santoku/matrix.lua
+++ test/spec/santoku/matrix.lua
@@ -15,3 +15,10 @@ mtx.add(m0, -1)
 for i = 1, 5 do
   assert(mtx.get(m0, 1, i) == i - 1)
 end
+
+m0 = mtx.create({ 1, 2, 3, 4, 5, 6 }, 2, 4)
+assert(mtx.rows(m0) == 1)
+assert(mtx.columns(m0) == 3)
+assert(mtx.get(m0, 1, 1) == 2)
+assert(mtx.get(m0, 1, 2) == 3)
+assert(mtx.get(m0, 1, 3) == 4)
