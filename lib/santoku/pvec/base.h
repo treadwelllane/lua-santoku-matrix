26d341e737e6fe4f86ae20c7dab2e40569555d3f
Matthew Brooks <mb964033@gmail.com>
2025-07-01 02:56:38 +0000

    add heap functions

--- lib/santoku/pvec/base.h
+++ lib/santoku/pvec/base.h
@@ -35,5 +35,51 @@ static inline void tk_pvec_hdesc (tk_pvec_t *v, tk_pair_t r)
   }
 }
 
+static inline void tk_pvec_hdesc_init (tk_pvec_t *v) {
+  ks_heapmake(tk_pvec_desc, v->n, v->a);
+}
+
+static inline void tk_pvec_hasc_init (tk_pvec_t *v) {
+  ks_heapmake(tk_pvec_asc, v->n, v->a);
+}
+
+static inline tk_pair_t tk_pvec_hdesc_pop (tk_pvec_t *v) {
+  tk_pair_t top = v->a[0];
+  v->a[0] = v->a[--v->n];
+  ks_heapadjust(tk_pvec_desc, 0, v->n, v->a);
+  return top;
+}
+
+static inline tk_pair_t tk_pvec_hasc_pop (tk_pvec_t *v) {
+  tk_pair_t top = v->a[0];
+  v->a[0] = v->a[--v->n];
+  ks_heapadjust(tk_pvec_asc, 0, v->n, v->a);
+  return top;
+}
+
+static inline void tk_pvec_hasc_push (tk_pvec_t *v, tk_pair_t r) {
+  size_t i = v->n;
+  tk_pvec_push(v, r);
+  while (i && tk_pvec_lt(v->a[i], v->a[(i - 1) >> 1])) {
+    size_t p = (i - 1) >> 1;
+    tk_pair_t tmp = v->a[i];
+    v->a[i] = v->a[p];
+    v->a[p] = tmp;
+    i = p;
+  }
+}
+
+static inline void tk_pvec_hdesc_push (tk_pvec_t *v, tk_pair_t r) {
+  size_t i = v->n;
+  tk_pvec_push(v, r);
+  while (i && tk_pvec_gt(v->a[i], v->a[(i - 1) >> 1])) {
+    size_t p = (i - 1) >> 1;
+    tk_pair_t tmp = v->a[i];
+    v->a[i] = v->a[p];
+    v->a[p] = tmp;
+    i = p;
+  }
+}
+
 #endif
 
