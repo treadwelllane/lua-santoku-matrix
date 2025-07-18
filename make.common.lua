2d6b02eb26f3759027b5c22ba0acb564c82e91b2
Matthew Brooks <mb964033@gmail.com>
2025-07-18 17:42:10 +0000

    split out sanitize profile

---
+++ make.common.lua
@@ -0,0 +1,41 @@
+local env = {
+
+  name = "santoku-matrix",
+  version = "0.0.81-1",
+  variable_prefix = "TK_MATRIX",
+  license = "MIT",
+  public = true,
+
+  cflags = {
+    "-std=gnu11", "-Wall", "-Wextra",
+    "-Wstrict-overflow", "-Wsign-conversion", "-Wsign-compare",
+    "-I$(shell luarocks show santoku --rock-dir)/include/",
+    "-I$(shell luarocks show santoku-threads --rock-dir)/include/",
+  },
+
+  ldflags = {
+    "-lm", "-lpthread"
+  },
+
+  dependencies = {
+    "lua == 5.1",
+    "santoku >= 0.0.279-1",
+    "santoku-threads >= 0.0.7-1",
+  },
+
+  test = {
+    dependencies = {
+      "luacov == 0.15.0-1",
+    }
+  },
+
+}
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
+
+return {
+  type = "lib",
+  env = env,
+}
