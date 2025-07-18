082db5e1ab2459e14c0177dd9b9af127a56221dd
broma0 <121064585+broma0@users.noreply.github.com>
2025-07-18 17:42:10 +0000

    extract common make configuration

--- make.lua
+++ make.lua
@@ -1,42 +1,6 @@
-local env = {
-
-  name = "santoku-matrix",
-  version = "0.0.81-1",
-  variable_prefix = "TK_MATRIX",
-  license = "MIT",
-  public = true,
-
-  cflags = {
-    "-O3", "-march=native",
-    "-std=gnu11", "-Wall", "-Wextra",
-    "-Wstrict-overflow", "-Wsign-conversion", "-Wsign-compare",
-    "-I$(shell luarocks show santoku --rock-dir)/include/",
-    "-I$(shell luarocks show santoku-threads --rock-dir)/include/",
-  },
-
-  ldflags = {
-    "-O3", "-march=native", "-lm", "-lpthread"
-  },
-
-  dependencies = {
-    "lua == 5.1",
-    "santoku >= 0.0.279-1",
-    "santoku-threads >= 0.0.7-1",
-  },
-
-  test = {
-    dependencies = {
-      "luacov == 0.15.0-1",
-    }
-  },
-
-}
-
-env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
-env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
-env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
-
-return {
-  type = "lib",
-  env = env,
-}
+local arr = require("santoku.array")
+local fs = require("santoku.fs")
+local base = fs.runfile("make.common.lua")
+base.env.cflags = arr.extend({ "-O3", "-march=native" }, base.env.cflags)
+base.env.ldflags = arr.extend({ "-O3", "-march=native" }, base.env.ldflags)
+return base
