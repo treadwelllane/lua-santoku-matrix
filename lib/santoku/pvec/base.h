39736043f9e77432ab054b22250baf6e0b58e28a
Matthew Brooks <mb964033@gmail.com>
2025-07-30 18:03:58 +0000

    update heap functions

--- lib/santoku/pvec/base.h
+++ lib/santoku/pvec/base.h
@@ -11,53 +11,53 @@ typedef struct { int64_t i; int64_t p; } tk_pair_t;
 #define tk_vec_limited
 #include <santoku/vec/tpl.h>
 
-static inline void tk_pvec_hasc (tk_pvec_t *v, tk_pair_t r)
-{
-  if (v->n < v->m) {
-    v->a[v->n ++] = r;
-    if (v->n == v->m)
-      ks_heapmake(tk_pvec_asc, v->m, v->a);
-  } else if (r.p < v->a[0].p) {
-    v->a[0] = r;
-    ks_heapadjust(tk_pvec_asc, 0, v->m, v->a);
-  }
-}
-
-static inline void tk_pvec_hdesc (tk_pvec_t *v, tk_pair_t r)
+static inline void tk_pvec_hmax (tk_pvec_t *v, tk_pair_t r)
 {
   if (v->n < v->m) {
     v->a[v->n ++] = r;
     if (v->n == v->m)
       ks_heapmake(tk_pvec_desc, v->m, v->a);
-  } else if (r.p > v->a[0].p) {
+  } else if (r.p < v->a[0].p) {
     v->a[0] = r;
     ks_heapadjust(tk_pvec_desc, 0, v->m, v->a);
   }
 }
 
-static inline void tk_pvec_hdesc_init (tk_pvec_t *v) {
+static inline void tk_pvec_hmin (tk_pvec_t *v, tk_pair_t r)
+{
+  if (v->n < v->m) {
+    v->a[v->n ++] = r;
+    if (v->n == v->m)
+      ks_heapmake(tk_pvec_asc, v->m, v->a);
+  } else if (r.p > v->a[0].p) {
+    v->a[0] = r;
+    ks_heapadjust(tk_pvec_asc, 0, v->m, v->a);
+  }
+}
+
+static inline void tk_pvec_hmax_init (tk_pvec_t *v) {
   ks_heapmake(tk_pvec_desc, v->n, v->a);
 }
 
-static inline void tk_pvec_hasc_init (tk_pvec_t *v) {
+static inline void tk_pvec_hmin_init (tk_pvec_t *v) {
   ks_heapmake(tk_pvec_asc, v->n, v->a);
 }
 
-static inline tk_pair_t tk_pvec_hdesc_pop (tk_pvec_t *v) {
+static inline tk_pair_t tk_pvec_hmax_pop (tk_pvec_t *v) {
   tk_pair_t top = v->a[0];
   v->a[0] = v->a[--v->n];
   ks_heapadjust(tk_pvec_desc, 0, v->n, v->a);
   return top;
 }
 
-static inline tk_pair_t tk_pvec_hasc_pop (tk_pvec_t *v) {
+static inline tk_pair_t tk_pvec_hmin_pop (tk_pvec_t *v) {
   tk_pair_t top = v->a[0];
   v->a[0] = v->a[--v->n];
   ks_heapadjust(tk_pvec_asc, 0, v->n, v->a);
   return top;
 }
 
-static inline void tk_pvec_hasc_push (tk_pvec_t *v, tk_pair_t r) {
+static inline void tk_pvec_hmax_push (tk_pvec_t *v, tk_pair_t r) {
   size_t i = v->n;
   tk_pvec_push(v, r);
   while (i && tk_pvec_lt(v->a[i], v->a[(i - 1) >> 1])) {
@@ -69,7 +69,7 @@ static inline void tk_pvec_hasc_push (tk_pvec_t *v, tk_pair_t r) {
   }
 }
 
-static inline void tk_pvec_hdesc_push (tk_pvec_t *v, tk_pair_t r) {
+static inline void tk_pvec_hmin_push (tk_pvec_t *v, tk_pair_t r) {
   size_t i = v->n;
   tk_pvec_push(v, r);
   while (i && tk_pvec_gt(v->a[i], v->a[(i - 1) >> 1])) {
@@ -82,4 +82,3 @@ static inline void tk_pvec_hdesc_push (tk_pvec_t *v, tk_pair_t r) {
 }
 
 #endif
-
