813ca3e76d86a3ba557abb0581202039248456b7
Matthew Brooks <mb964033@gmail.com>
2025-08-07 00:59:16 -0400

    wip

--- lib/santoku/rvec.h
+++ lib/santoku/rvec.h
@@ -7,6 +7,9 @@
 #define tk_vec_base tk_rank_t
 #define tk_vec_lt(a, b) ((a).d < (b).d)
 #define tk_vec_gt(a, b) ((a).d > (b).d)
+#define tk_vec_ltx(a,b)  ((a).i < (b).i || ((a).i == (b).i && (a).d < (b).d))
+#define tk_vec_gtx(a,b)  ((a).i > (b).i || ((a).i == (b).i && (a).d > (b).d))
+#define tk_vec_eqx(a, b) ((a).i == (b).i && (a).d == (b).d)
 #define tk_vec_limited
 #include <santoku/vec/ext/tpl.h>
 
