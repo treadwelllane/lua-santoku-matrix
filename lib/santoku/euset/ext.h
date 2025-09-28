c345dfbfd2b042b56073ba25f588a810331565c9
Matthew Brooks <mb964033@gmail.com>
2025-09-27 22:42:53 -0400

    wip

--- lib/santoku/euset/ext.h
+++ lib/santoku/euset/ext.h
@@ -1,6 +1,18 @@
 #ifndef TK_EUSET_EXT_H
 #define TK_EUSET_EXT_H
 
-// TODO
+#include <santoku/evec/base.h>
+
+static inline tk_evec_t *tk_euset_keys (lua_State *L, tk_euset_t *M)
+{
+  tk_evec_t *out = tk_evec_create(L, tk_euset_size(M), 0, 0);
+  tk_edge_t k;
+  out->n = 0;
+  tk_umap_foreach_keys(M, k, ({
+    out->a[out->n ++] = k;
+  }));
+  return out;
+}
+
 
 #endif
