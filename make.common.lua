7f3a73350c619b99831c2fffce8c0efcce1a020d
broma0 <121064585+broma0@users.noreply.github.com>
2025-10-02 15:44:25 -0400

    update make.common.lua

--- make.common.lua
+++ make.common.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-matrix",
-  version = "0.0.121-1",
+  version = "0.0.122-1",
   variable_prefix = "TK_MATRIX",
   license = "MIT",
   public = true,
@@ -19,8 +19,8 @@ local env = {
 
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.284-1",
-    "santoku-threads >= 0.0.13-1",
+    "santoku >= 0.0.285-1",
+    "santoku-threads >= 0.0.14-1",
   },
 
   test = {
