ae51b395e8bc187f6b5bd358c0e8c1bc2583f11c
broma0 <121064585+broma0@users.noreply.github.com>
2025-10-20 16:45:37 -0400

    update umap get and put return values

--- make.common.lua
+++ make.common.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-matrix",
-  version = "0.0.137-1",
+  version = "0.0.138-1",
   variable_prefix = "TK_MATRIX",
   license = "MIT",
   public = true,
@@ -19,7 +19,7 @@ local env = {
 
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.289-1",
+    "santoku >= 0.0.290-1",
     "santoku-threads >= 0.0.17-1",
   },
 
