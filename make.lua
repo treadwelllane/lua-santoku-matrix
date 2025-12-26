01ebe5f8f16475c17ed89c63d720d68a1dbab5c1
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-26 15:38:41 -0500

    bump version to 0.0.205-1

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local arr = require("santoku.array")
 local fs = require("santoku.fs")
 local base = fs.runfile("make.common.lua")
-base.env.cflags = arr.flatten({ { "-O3", "-march=native", "-fdata-sections", "-ffunction-sections" }, base.env.cflags or {} })
-base.env.ldflags = arr.flatten({ { "-O3", "-march=native", "-Wl,--gc-sections" }, base.env.ldflags or {} })
+base.env.cflags = arr.flatten({ { "-O3", "-fdata-sections", "-ffunction-sections" }, base.env.cflags or {} })
+base.env.ldflags = arr.flatten({ { "-O3", "-Wl,--gc-sections" }, base.env.ldflags or {} })
 return base
