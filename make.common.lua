463e0f723bd7a61650bad56b60b9102f0d3426a2
broma0 <121064585+broma0@users.noreply.github.com>
2026-01-24 18:20:33 -0500

    bump santoku dependency

--- make.common.lua
+++ make.common.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-matrix",
-  version = "0.0.229-1",
+  version = "0.0.230-1",
   variable_prefix = "TK_MATRIX",
   license = "MIT",
   public = true,
@@ -26,7 +26,7 @@ local env = {
   },
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.316-1",
+    "santoku >= 0.0.320-1",
   },
 }
 
